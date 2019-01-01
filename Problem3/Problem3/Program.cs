b12086471c30b36f03514a36011463c855fdbf78
PedroPini <augustopini@gmail.com>
2019-01-01 00:53:13 -0200

    Project Euler adding problem 3 solved

---
+++ Problem3/Problem3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = 600851475143;
+             for (int i = 1; i< number; i++)
+            {
+                if (number % i == 0)
+                {
+                    number = number / i;
+                }
+            }
+
+            Console.WriteLine(number);
+            Console.ReadLine();
+        }
+    }
+}
