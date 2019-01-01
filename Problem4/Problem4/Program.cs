12ea30443464b2639a594a8a2731122b9c5e1982
PedroPini <augustopini@gmail.com>
2019-01-01 13:53:11 -0200

    Adding problem 4 solution

---
+++ Problem4/Problem4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool found = false;
+            int firstHalf = 998, palin = 0;
+            int[] factors = new int[2];
+
+            while (!found)
+            {
+                firstHalf--;
+                palin = isPalindrome(firstHalf);
+                for (int i = 999; i > 99; i--)
+                {
+                    if ((palin / i) > 999 || i * i < palin)
+                    {
+                        break;
+                    }
+
+                    if ((palin % i == 0))
+                    {
+                        found = true;
+                        factors[0] = palin / i;
+                        factors[1] = i;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Found the palindrom {0}, which is made from {1}*{2}", palin, factors[0], factors[1]);
+            Console.ReadLine();
+        }
+        public static int isPalindrome(int firstHalf)
+        {
+            char[] reversed = firstHalf.ToString().Reverse().ToArray();
+            return Convert.ToInt32(firstHalf + new string(reversed));
+        }
+    }        
+}
